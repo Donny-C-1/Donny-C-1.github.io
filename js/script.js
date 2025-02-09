2597ce324a42a86298394843103655cca3ec7353
Donny C <chikwemdonald@gmail.com>
2025-02-10 00:48:57 +0100

    Added some projects

--- js/script.js
+++ js/script.js
@@ -11,4 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Footer year
   document.querySelector(".year").textContent = new Date().getFullYear();
+
+  const projectCards = document.querySelectorAll(".project-card");
+
+  projectCards.forEach((card) => {
+    card.addEventListener("mouseenter", () => {
+      card.style.transform = "scale(1.05)";
+    });
+
+    card.addEventListener("mouseleave", () => {
+      card.style.transform = "scale(1)";
+    });
+  });
 });
